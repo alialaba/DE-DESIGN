860322d5287e1756b27e908f743e3e8821250745
alialaba <alialaba079@gmail.com>
2023-05-12 17:26:20 -0400

    ft: implement overlay img hover effect and condition display

--- JS/products.js
+++ JS/products.js
@@ -14,27 +14,51 @@ products.forEach((product)=>{
 const productElement = document.createElement("div");
 const productImgElement = document.createElement("img");
 const productNameElement = document.createElement("p");
-// const productLinkIcon = document.createElement("");
+const productLinkElement = document.createElement("a");
+const iconElement = document.createElement('i');
+const productOverlayElement = document.createElement("div");
+const productContentElement = document.createElement("div");
 
 
 
 //Assign data content
 productNameElement.textContent = product.name;
+// productLinkElement.textContent = "he"
 productImgElement.src = product.imgCover;
 productImgElement.alt = product.name;
+
+
 
 //Assign classNames
 productElement.className ="product__item"
 productImgElement.className ="product__img"
 productNameElement.className ="product__name"
+productOverlayElement.className ="product__overlay"
+productContentElement.className = "product__content"
 
 
+
+
+if(product.done){
+    // diplay link icon
+    iconElement.classList.add("fa-solid", "fa-arrow-up");
+    productLinkElement.href = product.name
+    productLinkElement.classList.add("product__link", "product__link--light");
+}else{
+    //display lock icon
+    iconElement.classList.add("fa-solid", "fa-lock");
+    productLinkElement.classList.add("product__link", "product__link--dark");
+}
 
 
 //AppendChild to parent Elements
-productsList.appendChild(productElement)
-productElement.appendChild(productNameElement)
-productElement.appendChild(productImgElement)
+productsList.appendChild(productElement);
+productElement.appendChild(productOverlayElement);
+productElement.appendChild(productImgElement);
+productElement.appendChild(productContentElement);
+productContentElement.appendChild(productNameElement);
+productContentElement.appendChild(productLinkElement);
+productLinkElement.appendChild(iconElement)
 
 
 })
