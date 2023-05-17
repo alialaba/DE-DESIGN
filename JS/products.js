a069ba35d30987ea0c42e5d5207f149580661d55
alialaba <alialaba079@gmail.com>
2023-05-17 15:31:39 -0400

    embed link fix

--- JS/products.js
+++ JS/products.js
@@ -14,23 +14,22 @@ products.forEach((product)=>{
 const productElement = document.createElement("article");
 const productImgElement = document.createElement("img");
 const productNameElement = document.createElement("p");
-const productLinkElement = document.createElement("a");
+const productLinkIconElement = document.createElement("a");
+const productLinkElement = document.createElement("a")
 const iconElement = document.createElement('i');
 const productOverlayElement = document.createElement("div");
 const productContentElement = document.createElement("div");
+const productColTextElement = document.createElement("div");
+const productColIconElement = document.createElement("div")
+const productComingSoon = document.createElement("p");
 
 
 // const productElem = document.createElement("div");
 
-
-
 //Assign data content
 productNameElement.textContent = product.name;
-// productLinkElement.textContent = "he"
 productImgElement.src = product.imgCover;
 productImgElement.alt = product.name;
-
-
 
 //Assign classNames
 productElement.className ="product__item"
@@ -39,39 +38,43 @@ productNameElement.className ="product__name"
 productOverlayElement.className ="product__overlay"
 productContentElement.className = "product__content"
 
-
-
 //Conditional Link Icon display
 if(product.done){
-    // diplay link icon
+    // display link icon
     iconElement.classList.add("fa-solid", "fa-arrow-up");
-    productLinkElement.classList.add("product__link", "product__link--light");
+    productLinkIconElement.classList.add("product__link", "product__link--light");
+    productLinkElement.className ="product__link-overlay"
 
     // Create a click event listener for the product link element
-    productLinkElement.addEventListener("click", function(){
-    console.log("clicked")
+    productLinkElement.addEventListener("click", function(){window.location.href = `product.html?id=${product.id}`});
+    productLinkIconElement.addEventListener("click", function(){window.location.href = `product.html?id=${product.id}`})
 
-    window.location.href = `product.html?id=${product.id}`;  
-})
 }else{
     //display lock icon
     iconElement.classList.add("fa-solid", "fa-lock");
-    productLinkElement.classList.add("product__link", "product__link--dark");
+    productLinkIconElement.classList.add("product__link", "product__link--dark");
+
+    productComingSoon.className = "product__coming-soon"
+    productComingSoon.textContent ="Coming soon";
+    productColTextElement.appendChild(productComingSoon);
 }
-
-
-// pro.appendChild(productElem);
-
 
 
 //AppendChild to parent Elements
 productsList.appendChild(productElement);
-productElement.appendChild(productOverlayElement);
+// productElement.appendChild(productOverlayElement);
+productElement.append(productLinkElement);
+productLinkElement.appendChild(productOverlayElement)
+
 productElement.appendChild(productImgElement);
 productElement.appendChild(productContentElement);
-productContentElement.appendChild(productNameElement);
-productContentElement.appendChild(productLinkElement);
-productLinkElement.appendChild(iconElement)
+// productContentElement.appendChild(productNameElement);
+// productContentElement.appendChild(productLinkIconElement);
+productContentElement.appendChild(productColTextElement);
+productContentElement.appendChild(productColIconElement);
+productColTextElement.appendChild(productNameElement);
+productColIconElement.appendChild(productLinkIconElement);
+productLinkIconElement.appendChild(iconElement)
 
 
 })
